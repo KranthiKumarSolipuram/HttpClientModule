b3657d2a47f5f9cda02ec4ee92b93d6067f53cd7
Kranthi Kumar Solipuram <kranthikumar.solipuram@snaddevelopers.com>
2024-12-09 19:48:01 +0530

    HttpClientModule Intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from "@angular/common/http"
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
